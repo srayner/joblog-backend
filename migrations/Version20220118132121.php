2957a59a02ce60b3c73096913da6a470718b07f9
Stephen Rayner <stephen.rayner@marmalade.co.uk>
2022-01-18 18:08:43 +0000

    fixed broken migration

--- migrations/Version20220118132121.php
+++ migrations/Version20220118132121.php
@@ -21,7 +21,7 @@ final class Version20220118132121 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE job ADD user_id INT DEFAULT NOT NULL');
+        $this->addSql('ALTER TABLE job ADD user_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE job ADD CONSTRAINT FK_FBD8E0F8A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('CREATE INDEX IDX_FBD8E0F8A76ED395 ON job (user_id)');
         $this->addSql('INSERT INTO users (`first_name`, `last_name`) VALUES ("Steve", "Rayner")');
